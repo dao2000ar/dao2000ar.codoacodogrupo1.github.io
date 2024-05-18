8688c8e4a2ce453099db5bc52712a5ec8f31df30
nvaccarezza <nati.estudio@gmail.com>
2024-05-18 18:46:21 -0300

    modificaciones 1

--- js/js.js
+++ js/js.js
@@ -1,4 +1,9 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
+    var logo = document.querySelector("logo");
+
+
     header.classList.toggle("abajo",window.scrollY>0);
+    logo.classList.toggle("abajo",window.scrollY>0);
+
 })
